5b42e63129f0d09d3a50c2c82f9ad7b49d575c1b
Shashankdepala9908 <Shashankdepala9908@gmail.com>
2025-08-03 01:12:14 -0400

    resolving rebase errors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,30 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAuth, OidcSecurityService } from 'angular-auth-oidc-client';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAuth({
+      config: {
+        authority: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_BC1xE6x21',
+        redirectUrl: 'http://localhost:4200/',
+        postLogoutRedirectUri: 'http://localhost:4200/',
+        clientId: '5tudoi21vbi6cvu08rs0nnk6sa',
+        scope: 'openid email phone',
+        responseType: 'code',
+        silentRenew: true,
+        useRefreshToken: true,
+      }
+    }),
+  ],
+}).then(appRef => {
+  const oidc = appRef.injector.get(OidcSecurityService);
+  oidc.checkAuth().subscribe(result => {
+    console.log('CheckAuth result:', result);
+  });
+});
